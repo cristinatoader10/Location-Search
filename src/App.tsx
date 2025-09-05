103e5c03c239aac8d28076266c436aa6fd1564fb
Crisa <c.toader@student.utwente.nl>
2025-09-05 11:14:11 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return  <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+          Hellooo
+      </h1>
+  </div>
+}
+
+export default App
